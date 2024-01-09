f0479fc582490463865d437c42261da2fcc46385
madhunivi77 <madhunivi77@gmail.com>
2024-01-09 15:51:55 -0700

    completed all the visualisation graphs

--- generate_flower.py
+++ generate_flower.py
@@ -1,7 +1,8 @@
 # Author      : Madhumitha Sukumar
-# Time period : 3 Jan 2023 - 
-# Description : 
-
+# Time period : 3 Jan 2023 - 9 Jan 2023
+# Description : This Python script performs data cleaning, analyzes the Iris dataset 
+#               through visualizations, and implements a Decision Tree classifier, 
+#               providing key insights and model evaluation metrics
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score,classification_report
@@ -24,23 +25,28 @@ def printline():
 # Print the title and the number of rows and columns
 rows , columns = iris_data.shape
 console = Console() # Create a Console object
-console.print("\t\t\t[bold red]FLOWER PREDICTION USING IRIS DATA[/bold red]\n")
-printline()
+console.print("\n\t\t\t[bold red]FLOWER PREDICTION USING IRIS DATA[/bold red]\n")
 print("\nData Analysis on Iris dataset \n")
 print(f"Total Rows : {rows} \tColumns : {columns}")
+#print(iris_data.head()) -- top 5 rows
+#print(df.info()) -- column data types
 
 # Data cleaning
+
 # 1. Missing values
-print("\nMissing values ",iris_data.isnull().sum())
+print("\nMissing values\n",iris_data.isnull().sum())
 print()
 
 # 2. Duplicate Values
-duplicates = iris_data.duplicated()
-print("Number of duplicate rows:", duplicates.sum(),"\n")
-printline()
-iris_data = iris_data.drop_duplicates()#remove if any
+data = iris_data.drop_duplicates(subset ="species",)
+data
 
-#PART 2
+# 3. Balanced data set
+# species contain equal amounts of rows or not
+print(iris_data.value_counts("species")) 
+printline()
+
+#PART 2 - Data Science Task ( ML Algorithm - Decision trees)
 
 # Prepare the features (X) and target variable (y)
 X = iris_data.drop('species', axis=1)
@@ -74,3 +80,36 @@ printline()
 console.print("\n[yellow]Classification Report:[yellow]\n")
 print(classification_report(y_test, y_pred))
 printline()
+
+# PART 3 - Exploratory Data Analysis (EDA)
+
+# Display a countplot of the Species
+sns.countplot(x='species', data= iris_data, )
+plt.show()
+
+# Creating histograms for each category
+iris_data.hist(bins=20, figsize=(10, 7))
+plt.suptitle("Distribution of Features in Iris Dataset", y=0.99, fontsize=16)
+plt.show()
+
+# Scatterplot for Sepal
+sns.scatterplot(x='sepal_length', y='sepal_width',
+                hue='species', data=iris_data, )
+plt.legend(bbox_to_anchor=(0.67,0.99), loc=2)
+plt.show()
+
+# Scatterplot for Petal
+sns.scatterplot(x='petal_length', y='petal_width',
+                hue='species', data=iris_data, )
+plt.legend(bbox_to_anchor=(0.67,0.3), loc=2)
+plt.show()
+
+# Box plots to visualize the distribution of each feature by species
+plt.figure(figsize=(10, 7))
+for i, feature in enumerate(column_names[:-1]):
+    plt.subplot(2, 2, i + 1)
+    sns.boxplot(x='species', y=feature, data=iris_data ,palette='Set2')
+    plt.title(f'{feature} distribution by species')
+plt.suptitle("Box Plots of Features by Species", y=0.99, fontsize=16)
+plt.tight_layout()
+plt.show()
